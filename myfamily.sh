806cb8f9edfa77b4feb21daed23095ee7298f1e3
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-15 23:56:46 +0600

    adding sed

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq  " .[] | select( .id==$HERO_ID) | .connections | .relatives"
+curl -s  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq  " .[] | select( .id==$HERO_ID) | .connections | .relatives" | sed -e 's/"//g'
