5bd8547af9ed218e5601a3f80a44d899dfe1e06b
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-19 17:27:22 +0600

    print comb 2

--- printcomb2.go
+++ printcomb2.go
@@ -3,10 +3,10 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			z01.PrintRune(rune(i))
-			z01.PrintRune(rune(j))
+	for i := '0'; i < '9'; i++ {
+		for j := '0'; j < '9'; j++ {
+			z01.PrintRune(i)
+			z01.PrintRune(j)
 			z01.PrintRune(44)
 			z01.PrintRune(32)
 		}
