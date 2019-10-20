2abaf348ed94bc261fdafc53c0e4b78b8c4a73d6
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-20 23:58:14 +0600

    Printcomb 2

--- printcomb2.go
+++ printcomb2.go
@@ -5,12 +5,11 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i < '9'; i++ {
 		for j := '0'; j < '9'; j++ {
-			z01.PrintRune(i)
-			z01.PrintRune(j)
-			z01.PrintRune(44)
-
 			for k := '0'; k < '9'; k++ {
 				for l := '0'; l < '9'; l++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(32)
 					z01.PrintRune(k)
 					z01.PrintRune(l)
 					z01.PrintRune(44)
