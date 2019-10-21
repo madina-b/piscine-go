25d183fe028cc54f0b392476d1b126ea5c23948c
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-22 00:13:26 +0600

    find next prime

--- findnextprime.go
+++ findnextprime.go
@@ -1,7 +1,20 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb > 1 {
+	if nb > 0 {
+		if nb != 2 && nb%2 == 0 {
+			nb = nb + 1
+			FindNextPrime(nb)
+		} else if nb != 3 && nb%3 == 0 {
+			nb = nb + 1
+			FindNextPrime(nb)
+		} else if nb != 5 && nb%5 == 0 {
+			nb = nb + 1
+			FindNextPrime(nb)
+		} else if nb != 7 && nb%7 == 0 {
+			nb = nb + 1
+			FindNextPrime(nb)
+		}
 		for i := 2; i <= nb; i++ {
 			if i < nb && nb%i == 0 {
 				nb = nb + 1
