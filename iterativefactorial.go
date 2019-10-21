7d3fb30ab1a6235744fdd025beea92d244c6123a
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-21 12:22:21 +0600

    iterative factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,15 +1,16 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
+	var a int
 	if nb == 0 || nb == 1 {
-		arg = 1
+		a = 1
 	} else if nb > 0 {
 		for i := 1; i <= nb; i++ {
-			arg = arg * i
+			a = a * i
 		}
 
 	} else {
-		arg = 0
+		a = 0
 	}
-	return arg
+	return a
 }
