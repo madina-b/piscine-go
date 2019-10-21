048eb5cba4899a0851d498701317df97cdc372ee
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-22 00:35:52 +0600

    find next prime

--- findnextprime.go
+++ findnextprime.go
@@ -1,32 +1,24 @@
 package piscine
 
-func FindNextPrime(nb int) int {
+func IsPrime(nb int) bool {
 	if nb > 0 {
-		if nb != 2 && nb%2 == 0 {
-			nb = nb + 1
-			FindNextPrime(nb)
-		} else if nb != 3 && nb%3 == 0 {
-			nb = nb + 1
-			FindNextPrime(nb)
-		} else if nb != 5 && nb%5 == 0 {
-			nb = nb + 1
-			FindNextPrime(nb)
-		} else if nb != 7 && nb%7 == 0 {
-			nb = nb + 1
-			FindNextPrime(nb)
-		}
 		for i := 2; i <= nb; i++ {
 			if i < nb && nb%i == 0 {
-				nb = nb + 1
-				FindNextPrime(nb)
-			} else if i == nb {
-				return nb
+				return false
+			} else if nb%i == 0 {
+				return true
 			}
-
 		}
-
-	} else {
-		return 1
 	}
-	return nb
+
+	return false
+}
+
+func FindNextPrime(nb int) int {
+	for i := nb; i <= 2*nb; i++ {
+		if IsPrime(i) {
+			return i
+		}
+	}
+	return 1
 }
