aef20bf23e9ac500938755cc027279c35b996984
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-21 13:16:49 +0600

    Recursive factotial

---
+++ recursivefactorial.go
@@ -0,0 +1,14 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	a := 1
+	if nb == 0 || nb == 1 {
+		return 1
+	} else if nb > 1 && nb < 20 {
+		return nb * RecursiveFactorial(nb-1)
+
+	} else {
+		return 0
+	}
+	return a
+}
