9469ddaf0dbba1b70edb4020c54a827007db57de
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-21 23:41:24 +0600

    find next prime

--- findnextprime.go
+++ findnextprime.go
@@ -4,9 +4,8 @@ func FindNextPrime(nb int) int {
 	if nb > 1 {
 		for i := 2; i <= nb; i++ {
 			if i < nb && nb%i == 0 {
-				for j := 0; j < 20; j++ {
-					nb = nb + 1
-				}
+				nb = nb + 1
+				FindNextPrime(nb)
 			} else if i == nb {
 				return nb
 			}
