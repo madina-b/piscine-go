1effdc8191f2b84b91ec3da917e5a56129d54c8c
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-21 12:56:56 +0600

    Ierative factotial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,10 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	a:=1
+	a := 1
 	if nb == 0 || nb == 1 {
 		a = 1
-	} else if nb > 0 && nb<20 {
+	} else if nb > 0 && nb < 20 {
 		for i := 1; i <= nb; i++ {
 			a = a * i
 		}
