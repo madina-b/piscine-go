912c96a5df37deb22c4473baf12a882bec703dc4
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-22 18:28:19 +0600

    Alpha count

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,13 @@
 package piscine
 
 func AlphaCount(str string) int {
+	length:=0
 	runes := []rune(str)
-	for _, letter := range runes {
-		length := length + 1
+	for j := range runes {
+		length = j
 	}
 	count := 0
-	for i := 0; i < length; i++ {
+	for i := 0; i < length+1; i++ {
 		if runes[i] >= 65 && runes[i] <= 90 {
 			count = count + 1
 		} else if runes[i] >= 97 && runes[i] <= 122 {
