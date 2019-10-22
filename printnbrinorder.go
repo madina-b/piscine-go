652b97241cd37f9b149e315f34df306922d23836
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-22 20:22:18 +0600

    Print Nbr in order

---
+++ printnbrinorder.go
@@ -0,0 +1,16 @@
+package piscine
+
+func PrintNbrInOrder(nb int) {
+	if nb > 0 {
+		digit := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		for i := nb; i > 0; i = i / 10 {
+			remainder := i % 10
+			digit[remainder] = digit[remainder] + 1
+		}
+		for i := 0; i < 10; i++ {
+			for j := 0; j < digit[i]; j++ {
+				fmt.Print(i)
+			}
+		}
+	}
+}
