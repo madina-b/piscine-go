f4e75e20deefea36b5d4d79b897179cfd1450fa3
MadB-ux <mbaimaganbetova@gmail.com>
2019-10-23 23:06:07 +0600

    Revparams

---
+++ revparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	lenArg := 0
+	for i := range argumets {
+		lenArg = i + 1
+	}
+	for i := lenArg - 1; i > 0; i-- {
+		for _, letter := range arguments[i] {
+			z01.PrintRune(letter)
+		}
+		z01.PrintRune('\n')
+	}
+}
